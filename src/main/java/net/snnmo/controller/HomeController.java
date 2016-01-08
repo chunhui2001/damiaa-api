c2fef4a1862d84734852a7ca2aee48b5cbdf9b3d
keesh <chunhui2001@gmail.com>
2016-01-08 16:38:10 +0800

    get user authentication (principal) in controller

--- src/main/java/net/snnmo/controller/HomeController.java
+++ src/main/java/net/snnmo/controller/HomeController.java
@@ -1,11 +1,18 @@
 package net.snnmo.controller;
 
+import javafx.print.Collation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by TTong on 16-1-8.
@@ -17,5 +24,20 @@ public class HomeController {
         ModelAndView mv = new ModelAndView("home/index");
         mv.addObject("message", "This is Spring MVC index page~");
         return mv;
+    }
+
+
+    @RequestMapping(value="/api/test/users", method = RequestMethod.GET)
+    public ResponseEntity<Collection<String>> testUsers() {
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        System.out.println(user.getUsername());
+
+        Collection<String> coll = new ArrayList<String>();
+        coll.add("keesh");
+        coll.add("jenny");
+        coll.add("tongtong");
+        return new ResponseEntity<Collection<String>>(coll, HttpStatus.OK);
     }
 }
